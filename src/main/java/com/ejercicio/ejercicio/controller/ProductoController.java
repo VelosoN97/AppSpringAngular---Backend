a9cd01ef36c6878f5c72bccd428b3a4c439183ef
Nicolas Veloso <nicolas.veloso0397@gmail.com>
2021-10-24 17:56:04 -0300

    Primer commit para el repositorio

---
+++ src/main/java/com/ejercicio/ejercicio/controller/ProductoController.java
@@ -0,0 +1,83 @@
+package com.ejercicio.ejercicio.controller;
+
+import com.ejercicio.ejercicio.dto.Mensaje;
+import com.ejercicio.ejercicio.dto.ProductoDto;
+import com.ejercicio.ejercicio.entity.Producto;
+import com.ejercicio.ejercicio.service.ProductoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/producto")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ProductoController {
+
+    @Autowired
+    ProductoService productoService;
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<Producto>> list(){
+        List<Producto> list = productoService.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Producto> getById(@PathVariable("id") int id){
+        if (!productoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        Producto producto = productoService.getOne(id).get();
+        return new ResponseEntity(producto, HttpStatus.OK);
+    }
+
+    @GetMapping("/detailname/{nombre}")
+    public ResponseEntity<Producto> getById(@PathVariable("nombre") String nombre){
+        if (!productoService.existsByNombre(nombre))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        Producto producto = productoService.getByNombre(nombre).get();
+        return new ResponseEntity(producto, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody ProductoDto productoDto){
+        if (StringUtils.isBlank(productoDto.getNombre()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if (productoDto.getPrecio()==null || productoDto.getPrecio()<0)
+            return new ResponseEntity(new Mensaje("El precio debe ser mayor que 0"), HttpStatus.BAD_REQUEST);
+        if (productoService.existsByNombre(productoDto.getNombre()))
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        Producto producto = new Producto(productoDto.getNombre(), productoDto.getPrecio());
+        productoService.save(producto);
+        return new ResponseEntity(new Mensaje("El producto ha sido creado"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody ProductoDto productoDto){
+        if (!productoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        if (productoService.existsByNombre(productoDto.getNombre()) && productoService.getByNombre(productoDto.getNombre()).get().getId() != id)
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        if (StringUtils.isBlank(productoDto.getNombre()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if (productoDto.getPrecio()<0)
+            return new ResponseEntity(new Mensaje("El precio debe ser mayor que 0"), HttpStatus.BAD_REQUEST);
+
+        Producto producto = productoService.getOne(id).get();
+        producto.setNombre(productoDto.getNombre());
+        producto.setPrecio(productoDto.getPrecio());
+        productoService.save(producto);
+        return new ResponseEntity(new Mensaje("El producto ha sido actualizado"), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        if (!productoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        productoService.delete(id);
+        return new ResponseEntity(new Mensaje("El producto ha sido eliminado"), HttpStatus.OK);
+    }
+}
